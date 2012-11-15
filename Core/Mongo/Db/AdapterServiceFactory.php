f8c0601454679b78e2a6144bb8ab34d3f7fda867
Gavin Li <craftgavin@gmail.com>
2012-11-15 15:26:24 +0800

    bug fiexed

--- Core/Mongo/Db/AdapterServiceFactory.php
+++ Core/Mongo/Db/AdapterServiceFactory.php
@@ -10,10 +10,9 @@ class AdapterServiceFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-    	$siteConfig = $serviceLocator->get('Fucms\SiteConfig'); 
-    	
         $config = $serviceLocator->get('Configuration');
         if($config['mongo_db']['dbName'] == 'cms') {
+        	$siteConfig = $serviceLocator->get('Fucms\SiteConfig');
         	$config['mongo_db']['dbName'] = 'cms_'.$siteConfig->globalSiteId;
         }
         $adapter = new Adapter($config['mongo_db']);
