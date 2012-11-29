45b988d4399ca0519780d37ea8c7a4933f4bf38b
Gavin Li <craftgavin@gmail.com>
2012-11-29 23:13:59 +0800

    added content display after ajax post in front page admin

--- Core/Controller/Plugin/SwitchContext.php
+++ Core/Controller/Plugin/SwitchContext.php
@@ -5,16 +5,29 @@ use Zend\Mvc\Controller\Plugin\Redirect;
 
 class SwitchContext extends Redirect
 {
-	public function __invoke($controllerName = "index", $actionName = "index")
+	public function __invoke($controllerName = "index", $actionName = "index", $param = null, $redirect = false)
 	{
 		$event = $this->getEvent();
 		$request = $event->getRequest();
 		
 		if($request->isXmlHttpRequest()) {
 			$response = $this->getResponse();
-			$response->setContent("");
-			$response->getHeaders()->addHeaderLine('Content-Type', 'text/html');
-			$response->getHeaders()->addHeaderLine('result', 'success');
+			if($redirect) {
+				$controller = $this->getController();
+				$urlPlugin = $controller->plugin('url');
+				$params = array('controller' => $controllerName.'.ajax', 'action' => $actionName);
+				if(!is_scalar($param)) {
+					$params = array_merge($params, $param);
+				}
+				$url = $urlPlugin->fromRoute(null, $params);
+				$response->getHeaders()->addHeaderLine('Content-Type', 'text/html');
+				$response->getHeaders()->addHeaderLine('result', 'redirect');
+				$response->getHeaders()->addHeaderLine('url', $url);
+			} else {
+				$response->setContent("");
+				$response->getHeaders()->addHeaderLine('Content-Type', 'text/html');
+				$response->getHeaders()->addHeaderLine('result', 'success');
+			}
 			return $response;
 		} else {
 			if(is_array($controllerName)) {
